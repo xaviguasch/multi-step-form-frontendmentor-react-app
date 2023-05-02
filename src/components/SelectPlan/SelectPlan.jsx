da0d66dcd66381e0be8fdeadc5006a4e4b1679d4
Xavi Guasch <xaviguasch@gmail.com>
2023-05-02 12:44:01 +0200

    added partial styling to SelectPlan component, form pending to style

--- src/components/SelectPlan/SelectPlan.jsx
+++ src/components/SelectPlan/SelectPlan.jsx
@@ -5,6 +5,11 @@ import { MultiContext } from '../../context/MultiProvider'
 
 import { PLANS } from '../../utils/utils'
 
+// Imported svgs
+import { ReactComponent as IconArcadeSVG } from '../../assets/images/icon-arcade.svg'
+import { ReactComponent as IconAdvancedSVG } from '../../assets/images/icon-advanced.svg'
+import { ReactComponent as IconProSVG } from '../../assets/images/icon-pro.svg'
+
 const SelectPlan = () => {
   const { planData, setPlanData } = useContext(MultiContext)
 
@@ -28,87 +33,100 @@ const SelectPlan = () => {
       </div>
 
       <form className={classes.form}>
-        <fieldset>
+        <div className={classes.radioFormPlan}>
+          <div className={classes.selectPlanInd}>
+            <label htmlFor='arcade' className={classes.selectLabel}>
+              <input
+                type='radio'
+                className={classes.inputRadioCheckbox}
+                name='select-plan'
+                id='arcade'
+                value='arcade'
+                checked={plan === 'arcade'}
+                onChange={(e) => {
+                  setPlan(e.target.value)
+                }}
+              />
+              <IconArcadeSVG />
+              <div className={classes.spanGroup}>
+                <span>{PLANS.arcade.name}</span>
+                <span>{monthlyOrYearly === 'monthly' ? '$9/mo' : '$90/yr'}</span>
+                {monthlyOrYearly === 'yearly' && <span>2 months free</span>}
+              </div>
+            </label>
+          </div>
+
+          <div className={classes.selectPlanInd}>
+            <label htmlFor='advanced' className={classes.selectLabel}>
+              <input
+                type='radio'
+                className={classes.inputRadioCheckbox}
+                name='select-plan'
+                id='advanced'
+                value='advanced'
+                checked={plan === 'advanced'}
+                onChange={(e) => {
+                  setPlan(e.target.value)
+                }}
+              />
+              <IconAdvancedSVG />
+              <div className={classes.spanGroup}>
+                <span>{PLANS.advanced.name}</span>
+                <span>{monthlyOrYearly === 'monthly' ? '$12/mo' : '$120/yr'}</span>
+                {monthlyOrYearly === 'yearly' && <span>2 months free</span>}
+              </div>
+            </label>
+          </div>
+
+          <div className={classes.selectPlanInd}>
+            <label htmlFor='pro' className={classes.selectLabel}>
+              <input
+                type='radio'
+                className={classes.inputRadioCheckbox}
+                name='select-plan'
+                id='pro'
+                value='pro'
+                checked={plan === 'pro'}
+                onChange={(e) => {
+                  setPlan(e.target.value)
+                }}
+              />
+              <IconProSVG />
+
+              <div className={classes.spanGroup}>
+                <span>{PLANS.pro.name}</span>
+                <span>{monthlyOrYearly === 'monthly' ? '$15/mo' : '$150/yr'}</span>
+                {monthlyOrYearly === 'yearly' && <span>2 months free</span>}
+              </div>
+            </label>
+          </div>
+        </div>
+
+        <div className={classes.radioFormTime}>
           <input
             type='radio'
-            name='select-plan'
-            id='arcade'
-            value='arcade'
-            checked={plan === 'arcade'}
-            onChange={(e) => {
-              setPlan(e.target.value)
+            name='monthly-or-yearly'
+            id='monthly-sub'
+            value='monthly'
+            checked={monthlyOrYearly === 'monthly'}
+            onChange={(event) => {
+              setMonthlyOrYearly(event.target.value)
             }}
           />
-          <label htmlFor='arcade'>
-            <span>{PLANS.arcade.name}</span>
-            <span>{monthlyOrYearly === 'monthly' ? '$9/mo' : '$90/yr'}</span>
-            {monthlyOrYearly === 'yearly' && <span>2 months free</span>}
-          </label>
+          <label htmlFor='monthly-sub'>Monthly</label>
           <br />
 
           <input
             type='radio'
-            name='select-plan'
-            id='advanced'
-            value='advanced'
-            checked={plan === 'advanced'}
-            onChange={(e) => {
-              setPlan(e.target.value)
+            name='monthly-or-yearly'
+            id='yearly-sub'
+            value='yearly'
+            checked={monthlyOrYearly === 'yearly'}
+            onChange={(event) => {
+              setMonthlyOrYearly(event.target.value)
             }}
           />
-          <label htmlFor='advanced'>
-            <span>{PLANS.advanced.name}</span>
-            <span>{monthlyOrYearly === 'monthly' ? '$12/mo' : '$120/yr'}</span>
-            {monthlyOrYearly === 'yearly' && <span>2 months free</span>}
-          </label>
-          <br />
-
-          <input
-            type='radio'
-            name='select-plan'
-            id='pro'
-            value='pro'
-            checked={plan === 'pro'}
-            onChange={(e) => {
-              setPlan(e.target.value)
-            }}
-          />
-          <label htmlFor='pro'>
-            <span>{PLANS.pro.name}</span>
-            <span>{monthlyOrYearly === 'monthly' ? '$15/mo' : '$150/yr'}</span>
-            {monthlyOrYearly === 'yearly' && <span>2 months free</span>}
-          </label>
-        </fieldset>
-
-        <div>
-          <fieldset>
-            <legend>Do you agree?</legend>
-
-            <input
-              type='radio'
-              name='monthly-or-yearly'
-              id='monthly-sub'
-              value='monthly'
-              checked={monthlyOrYearly === 'monthly'}
-              onChange={(event) => {
-                setMonthlyOrYearly(event.target.value)
-              }}
-            />
-            <label htmlFor='monthly-sub'>Monthly</label>
-            <br />
-
-            <input
-              type='radio'
-              name='monthly-or-yearly'
-              id='yearly-sub'
-              value='yearly'
-              checked={monthlyOrYearly === 'yearly'}
-              onChange={(event) => {
-                setMonthlyOrYearly(event.target.value)
-              }}
-            />
-            <label htmlFor='yearly-sub'>Yearly</label>
-          </fieldset>
+          <label htmlFor='yearly-sub'>Yearly</label>
         </div>
       </form>
     </div>
